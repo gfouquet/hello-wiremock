708d8c6b3a505bd43e85cc5348634e29c74f8bb9
Grégory Fouquet <gfouquet@armida.fr>
2018-03-27 22:24:40 +0200

    Uuid generated by random-as-a-service. Test breaks

--- src/main/kotlin/com/example/demo/HelloController.kt
+++ src/main/kotlin/com/example/demo/HelloController.kt
@@ -3,6 +3,7 @@ package com.example.demo
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Mono
 
 @RestController
@@ -10,7 +11,16 @@ class HelloController {
 
     @GetMapping("hello/{name}")
     fun hello(@PathVariable name: String): Mono<Hello> {
-        return Mono.just(Hello("1", "Hello, $name !"))
+        return WebClient.create("http://dev-random-as-a-service.appspot.com/dev/urandom?count=8&io=text")
+            .get()
+
+            .retrieve()
+
+            .bodyToMono(String::class.java)
+            .map {
+                uuid -> Hello(uuid, "Hello, $name !")
+            }
+
     }
 }
 
